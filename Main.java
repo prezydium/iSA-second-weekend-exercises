c15579772ba7f6514d2b86642d8bdaf7a05d7608
prezydium <prezydium834@gmail.com>
2018-02-03 10:45:53 +0100

    https://www.codewars.com/kata/building-blocks/train/java

--- Main.java
+++ Main.java
@@ -5,8 +5,10 @@ public class Main {
 /*        Ghost ghost = new Ghost();
         System.out.println(ghost.getColor());*/
        // FindList list = new FindList();
-        Oddtriangle oddtriangle = new Oddtriangle();
-        System.out.println(oddtriangle.rowSumOddNumbers(42));
+       // Oddtriangle oddtriangle = new Oddtriangle();
+        //System.out.println(oddtriangle.rowSumOddNumbers(42));
 
+        Block b = new Block(new int[]{2,4,6});
+        System.out.println(b.getSurfaceArea());
     }
 }
